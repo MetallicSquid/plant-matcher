2363653199c9ab3cd7d1b0a1d0a2314eae97cb5e
MetallicSquid <gcmacneil@icloud.com>
2020-11-01 10:36:05 +0000

    Improved scientific information collection

--- concept_test/find_information.py
+++ concept_test/find_information.py
@@ -1,36 +1,37 @@
 # Script to find information about an plant in the wikipedia table
 import requests
-'''
-This script needs to find information about a given plant.
+from googlesearch import search
+from bs4 import BeautifulSoup
+import pandas as pd
+from time import sleep
 
-This means that I need to send a request to Wikipedia, trying the `plant string`.
-As I see it there are a few ways a given request could go:
-    1.  Request is not recieved
-    2.  Request is recieved - page doesn't exist
-    3.  Request is recieved - incorrect page is found
-    4.  Request is recieved - correct page is found
-The first two scenarios are easy to detect:
-    1.  Connection error occurs
-    2.  404 response
-The second two scenarios are a bit harder, I have to find a consistent pattern
-between them.
-'''
 
-def find_information(category_string):
+def find_sci_information(category_string):
     url = f"https://en.wikipedia.org/wiki/{category_string}"
-    # Test for case 1 - Request is not recieved
+    # Use wikipedia respectfully, give it 2 seconds to rest
+    sleep(2)
+
+    # Account for connection errors and 404s
     try:
         request = requests.get(url)
-
-        # Test for case 2 - Page doesn't exist
         if request.status_code == 404:
             return "Page not found"
-
-        # Test for case 3 - incorrect page is found
-
-        # Test for case 4 - correct page is found
-
     except requests.exceptions.ConnectionError:
         return "Connection failed - check your wifi"
 
-print(find_information("https://en.wikipedia.org/wiki/iuabwnopk"))
+    # Return scientific information about the given plant
+    try:
+        scientific = pd.read_html(url, header=1, index_col=0)[0]
+        scientific_dict = {}
+        for row in scientific.iterrows():
+            if "kingdom" in row[0].lower():
+                scientific_dict["Kingdom"] = row[1][0]
+            elif "family" in row[0].lower():
+                scientific_dict["Family"] = row[1][0]
+            elif "genus" in row[0].lower():
+                scientific_dict["Genus"] = row[1][0]
+
+        #print(scientific_dict)
+        return scientific_dict
+    except:
+        return f"No entry for {category_string}"
--- concept_test/process_data.py
+++ concept_test/process_data.py
@@ -1,6 +1,7 @@
 # Script to collect the data described in the 'Companion Plants' wikipedia page
 import pandas as pd
 from numpy import nan
+from find_information import find_sci_information
 
 # Scrape the tables and convert them to a dataframe
 data_list = pd.read_html(r"https://en.wikipedia.org/wiki/List_of_companion_plants", header=1, na_values="")
@@ -26,7 +27,12 @@ for column in dataframe.columns.values:
     dataframe[column] = dataframe[column].str.replace(r"&", ",")
     dataframe[column] = dataframe[column].str.replace(r"and", ",")
     dataframe[column] = dataframe[column].str.replace(r".", "")
-dataframe = dataframe.drop(["Scientific name", "Comments"], axis=1)
+dataframe = dataframe.drop(["Comments"], axis=1)
+
+# Make a dictionary to associate scientific to common names
+common_to_sci = {}
+for row in dataframe.iterrows():
+    common_to_sci[row[1][0]] = row[1][1]
 
 # Create comparison dictionaries for the relevant columns
 '''
@@ -34,25 +40,26 @@ dataframe = dataframe.drop(["Scientific name", "Comments"], axis=1)
     *   Iterate over list
     |--->   Check whether individual item has a wikipedia page
     |--->   Find relevant information on the item
-    |--->   Assign a key-value pair in the relevant dictionary for the item
+    |--->   Assign a key-value pair in the relevant dictionary
+            for the item
 '''
+name_dict = {}
 help_dict = {}
 helped_by_dict = {}
 attract_dict = {}
 repel_dict = {}
 avoid_dict = {}
 
-# 'Helps' column
-for item in dataframe.Helps:
+# The 'Name' columns
+for item in dataframe["Scientific name"]:
     # Split cell into list
     item.strip()
     current_list = []
     if item != '':
         current_list = item.split(",")
     for element in current_list:
-        pass
-        # Run search function on `element`
-
+        print(element)
+        print(find_sci_information(element.replace(" ", "_")))
 
 # 'Helped by' column
 # 'Attracts' column
